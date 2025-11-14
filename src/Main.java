d27e5549f0abbd8d59a10565ca81b275300eb256
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2025-11-14 18:52:40 +0530

    second commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
+
+//package is a folder in which your java file should lies
+//The meaning of the System.out.println is ->
+//system is a class in which out is a variable (which is  type of printStream )
+//and println is a method in the printStream class
